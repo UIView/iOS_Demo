3a3c7ce10c68c9d99ecc9e5e11417e4b7144879e
yahui <xlp604@163.com>
2016-11-14 17:28:41 +0800

    上传 JSPatch（热修复）for iOS Demo

---
+++ JSPatchDemo/JSPatchDemo/JPViewController.m
@@ -0,0 +1,31 @@
+//
+//  JPViewController.m
+//  JSPatch
+//
+//  Created by bang on 15/5/2.
+//  Copyright (c) 2015年 bang. All rights reserved.
+//
+
+#import "JPViewController.h"
+
+@implementation JPViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor whiteColor];
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 50)];
+    [btn setTitle:@"Push JPTableViewController" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(handleBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [btn setBackgroundColor:[UIColor grayColor]];
+    [self.view addSubview:btn];
+}
+
+- (void)handleBtn:(id)sender
+{
+    self.title=@"1988";
+    NSLog(@"我 开始远行 了");
+}
+
+@end
+
+
---
+++ JSPatchDemo/JSPatchDemo/NewTestViewController.h
@@ -0,0 +1,14 @@
+//
+//  NewTestViewController.h
+//  JSPatchDemo
+//
+//  Created by Yahui Duan on 16/11/14.
+//  Copyright © 2016年 bang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "JPViewController.h"
+
+@interface NewTestViewController : JPViewController
+
+@end
